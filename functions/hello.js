092649dc736481136ee99c207f61ebcdd8bee1d2
David Hill <dh366k@us.att.com>
2021-03-18 12:43:52 +0000

    Trying retuning an object

--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,8 @@ exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
     statusCode: 200, 
-    body: `Hello ${subject}`
+    body: {
+      isThisJson: `Hello ${subject}`
+    }
   }
 }
